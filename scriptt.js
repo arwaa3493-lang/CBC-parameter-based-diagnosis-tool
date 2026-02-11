08d506fad1512a4563583ce632e657bb5bc2e1fe
arwaa arebee <arwaa3493@gmail.com>
2026-02-11 16:07:13 +0300

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -34,51 +34,51 @@ diagnoseButton.addEventListener("click", function () {
   let result = "";
  
   if (hb < ranges.hb.low && rdw > ranges.rdw.high && mch < ranges.mch.low && mchc < ranges.mchc.low && rbc > ranges.rbc.high && mcv < ranges.mcv.low) {
-    result = " Hemoglobin H disese. confirmation done by supravital stains that show blue aggregates."
+    result = " Hemoglobin H disese. confirmation done by supravital stains that show blue aggregates. \n";
     }
-  else if (hb < ranges.hb.low && rbc < ranges.rbc.low && mcv > ranges.mcv.high && plt < ranges.plt.low) {
-    result = " Hemolytic uremic syndrome. confirmation: RFTs + Schistocytes in blood film." 
+ if (hb < ranges.hb.low && rbc < ranges.rbc.low && mcv > ranges.mcv.high && plt < ranges.plt.low) {
+    result = " Hemolytic uremic syndrome. confirmation: RFTs + Schistocytes in blood film.\n"; 
     }
-  else if (hb < ranges.hb.low && rbc < ranges.rbc.low && rdw > ranges.rdw.high && wbc > ranges.wbc.high) {
-  result = " If reticulocyte count is high + DAT is positive 'n Autoimmune hemolytic anemia. Otherwise, check blood film for possible leukemia." 
+ if (hb < ranges.hb.low && rbc < ranges.rbc.low && rdw > ranges.rdw.high && wbc > ranges.wbc.high) {
+  result = " If reticulocyte count is high + DAT is positive 'n Autoimmune hemolytic anemia. Otherwise, check blood film for possible leukemia.\n"; 
       }
 
-  else if (hb < ranges.hb.low && rbc < ranges.rbc.low) {
-    result = "Pattern suggests anemia due to blood loss or iron deficiency.";
+ if (hb < ranges.hb.low && rbc < ranges.rbc.low) {
+    result = "Pattern suggests anemia due to blood loss or iron deficiency.\n";
   } 
-  else if (hb > ranges.hb.high && rbc > ranges.rbc.high) {
-    result = "Possible high-altitude adaptation or polycythemia. Check blood film.";
+ if (hb > ranges.hb.high && rbc > ranges.rbc.high) {
+    result = "Possible high-altitude adaptation or polycythemia. Check blood film.\n";
   } 
-  else if (wbc > ranges.wbc.high) {
-    result = "Possible infection, inflammation, leukemia, or autoimmune disease. Check blood film.";
+ if (wbc > ranges.wbc.high) {
+    result = "Possible infection, inflammation, leukemia, or autoimmune disease. Check blood film.\n";
   }
-  else if (plt > ranges.plt.high) {
-    result = "Possible Essential Thrompocythemia. check blood film and clinical history."
+ if (plt > ranges.plt.high) {
+    result = "Possible Essential Thrompocythemia. check blood film and clinical history.\n";
   }
  
-  else if (plt < ranges.plt.low) {
-    result = " Most classically, Alcoholic liver disease, to confirm: check LFTs. Others: Immune Thrombocytopenic Purpura if the plt is extremely low. otherwise, thrompoctopenia due to viral illnes, medication, hypersplenism, or iron deficiency"
+ if (plt < ranges.plt.low) {
+    result = " Most classically, Alcoholic liver disease, to confirm: check LFTs. Others: Immune Thrombocytopenic Purpura if the plt is extremely low. otherwise, thrompoctopenia due to viral illnes, medication, hypersplenism, or iron deficiency.\n";
                                          }
 
-  else if (mcv > ranges.mcv.high) {
-    result = " Megaloblastic anemia, Alcohol abuse, liver disease, hypothyroidism, myelodysplastic syndrome. check clinicaal picture."
+ if (mcv > ranges.mcv.high) {
+    result = " Megaloblastic anemia, Alcohol abuse, liver disease, hypothyroidism, myelodysplastic syndrome. check clinicaal picture.\n";
 }
 
-  else if (mcv < ranges.mcv.low) {
-    result = "Iron deficiency, Thalassemia trait, harmless slight microcytosis. Check blood film." 
+ if (mcv < ranges.mcv.low) {
+    result = "Iron deficiency, Thalassemia trait, harmless slight microcytosis. Check blood film.\n"; 
 }
-  else if (mch > ranges.mch.high && mchc > ranges.mchc.high) {
-    result = "Hereditary spherocytosis. However, it is uncommon pattern. possible cold agglutinins. warm up the sample and run again."
+ if (mch > ranges.mch.high && mchc > ranges.mchc.high) {
+    result = "Hereditary spherocytosis. However, it is uncommon pattern. possible cold agglutinins. warm up the sample and run again.\n";
 }
-  else if (mchc < ranges.mch.low && mchc < ranges.mchc.low) {
-    result = "Hypochromia : Iron Deficiency or Thalassemia trait. If slight however, it is normal. check blood film." 
+ if (mchc < ranges.mch.low && mchc < ranges.mchc.low) {
+    result = "Hypochromia : Iron Deficiency or Thalassemia trait. If slight however, it is normal. check blood film.\n"; 
 }
-  else if (rdw > ranges.rdw.high) {
-    result = " Anisocytosis, variation of size between cells. Not a uniform shape." 
+ if (rdw > ranges.rdw.high) {
+    result = " Anisocytosis, variation of size between cells. Not a uniform shape.\n"; 
 }
 
   else {
-    result = "Values are within normal limits or inconclusive.";
+    result = "Values are within normal limits or inconclusive.\n";
   }
   
 
